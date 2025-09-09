7ab428237bee859ba7e78926fff330b28305c9c2
Hari G <hprasath145@gmail.com>
2025-09-09 22:16:24 +0530

    docs: add license URL references
    - Update copyright notices to include license URL reference

--- src/utils/validate.utils.ts
+++ src/utils/validate.utils.ts
@@ -1,7 +1,7 @@
 /*
  * The MIT License
  *
- * Copyright (c) 2025 Catbee Technologies
+ * Copyright (c) 2025 Catbee Technologies. https://catbee-utils.npm.hprasath.com/license
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
